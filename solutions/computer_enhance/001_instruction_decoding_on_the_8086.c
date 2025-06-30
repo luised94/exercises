532c63b32e7d8214909b71263b8c77e208425f28
luised94 <luised94@mit.edu>
2025-06-29 22:40:49 -0400

    feat(computer_enhance): Add errors, homework descriptions and first homework

---
+++ solutions/computer_enhance/001_instruction_decoding_on_the_8086.c
@@ -0,0 +1,6 @@
+/*
+Title: Instruction decoding on the 8086
+URL: https://www.computerenhance.com/p/instruction-decoding-on-the-8086
+Key: @muratorih, @zotero-item-155260
+Assemble the instructions into binary then decode them following the instructions provided by the 8086 manual.
+*/

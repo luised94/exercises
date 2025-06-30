d21e3bc273e97a832fa82f500ecc4a10cdf0d3bd
luised94 <luised94@mit.edu>
2025-06-29 22:52:46 -0400

    chore(computer_enhance): Add preprocessor directive.

--- solutions/computer_enhance/001_instruction_decoding_on_the_8086.c
+++ solutions/computer_enhance/001_instruction_decoding_on_the_8086.c
@@ -4,3 +4,5 @@ URL: https://www.computerenhance.com/p/instruction-decoding-on-the-8086
 Key: @muratorih, @zotero-item-155260
 Assemble the instructions into binary then decode them following the instructions provided by the 8086 manual.
 */
+
+#include <stdio.h>
